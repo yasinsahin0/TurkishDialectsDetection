0e381afe20a381f359be030fb8691d0e07a62b0c
Yasin Şahin <56588137+yasinsahin0@users.noreply.github.com>
2022-03-14 12:06:08 +0300

    kodinat

--- LinearRegresyon/kidem_maas.py
+++ LinearRegresyon/kidem_maas.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-dataset = pd.read_csv('Kidem_ve_Maas_VeriSeti.csv')
+dataset = pd.read_csv('data/Kidem_ve_Maas_VeriSeti.csv')
 
 X = dataset.iloc[:, :-1].values # tek sütun olsada bir matris onu vektöre çevirdik
 y = dataset.iloc[:, 1].values # bir vektör olduğu için stünü direk aldık
--- LinearRegresyon/kordinat.py
+++ LinearRegresyon/kordinat.py
@@ -4,20 +4,38 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-dataset = pd.read_csv('kordinat.csv')
+class PathPredict:
+    def __init__(self):
+        self.dataset = pd.read_csv('data/kordinat.csv')
+        self.X = self.dataset.iloc[:, :-1].values # tek sütun olsada bir matris onu vektöre çevirdik
+        self.y = self.dataset.iloc[:, 1].values # bir vektör olduğu için stünü direk aldık
 
-X = dataset.iloc[:, :-1].values # tek sütun olsada bir matris onu vektöre çevirdik
-y = dataset.iloc[:, 1].values # bir vektör olduğu için stünü direk aldık
+    def predict(self,value):
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size = 1/3, random_state = 0)
+        regressor = LinearRegression() # lineer reg sınıfından nesne oluşturuyoruz.
+        regressor.fit(X_train, y_train) # model eğitimi yapıyoruz
+        return regressor.predict(np.array([[value]]))
+
+nsne = PathPredict()
+
+la =[55,70,90]
+lb =[]
+
+genelx = []
+genely = []
+for i in la:
+    lb.append(nsne.predict(i))
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+genelx.append(int(nsne.X[0]))
+genelx.append(la[-1])
 
-regressor = LinearRegression() # lineer reg sınıfından nesne oluşturuyoruz.
 
-regressor.fit(X_train, y_train) # model eğitimi yapıyoruz
-#print(X_test)
-a = np.array([[65],[80]])
-print(a)
-y_pred = regressor.predict(a)
-print("X:65 te Tahmini Y değeri : "+str(y_pred[0]))
-print("X:80 te Tahmini Y değeri : "+str(y_pred[1]))
+genely.append(int(nsne.y[0]))
+genely.append(lb[-1])
+
+fig = plt.figure(figsize=(10,10))
+plt.plot(list(nsne.X),list(nsne.y)) # ilk parametre x ekseni ikinci parametre y ekseni
+plt.plot(la,lb,'r--')
+plt.plot(genelx,genely,'k')
+plt.show()
