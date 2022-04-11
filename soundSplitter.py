393211924524505d3a631f6a234daec45567dafc
Yasin Şahin <56588137+yasinsahin0@users.noreply.github.com>
2022-04-11 18:30:07 +0300

    fixed

--- soundSplitter.py
+++ soundSplitter.py
@@ -1,9 +1,10 @@
 import os
-from pydub import AudioSegment
+import librosa
 
-class SoundSplit:
+class SoundSplitter:
 
     def __init__(self):
+<<<<<<< HEAD
         self.sound_file_path = 'sound_download/'
         self.splitter_path = 'sound_split/sounds/'
         self.data_create_path = "sound_split/data.csv"
@@ -16,11 +17,29 @@ class SoundSplit:
             if file_name.find(".") > 0:
                 sound_file_list.remove(file_name)
         print(sound_file_list)
+=======
+        self.root_path = os.getcwd() + "/"
+        self.sound_path = self.root_path + "sound_download"
+        self.sound_split_file = self.root_path + "sound_split"
 
+    def SoundFileNameList(self):
+        file_list = os.listdir(self.sound_path)
+        for name in file_list:
+            if name.find(".") >= 0:
+                file_list.remove(name)
+        return file_list
+>>>>>>> parent of 1409bb4 (soundSplit)
 
+    
+
+<<<<<<< HEAD
 
 
 
 
 nesne = SoundSplit()
 nesne.sound_data_list_create()
+=======
+nesne = SoundSplitter()
+nesne.SoundFileNameList()
+>>>>>>> parent of 1409bb4 (soundSplit)
